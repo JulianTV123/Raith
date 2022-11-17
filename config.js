460df2d082fd67ea0981c70286dfd0f5705de2ee
Julian Tocasuche <julistv1234@gmail.com>
2022-11-17 11:04:07 -0500

    Musical Discord Bot Version 2.0

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+ dirBase : './database/base_1.db',
+ superusers: [], //Puedes añadir ID
+ token: 'ODI5MDE5NzU0MTMwOTY0NTMy.YGyCzw.MGYTjTwKITInWade-HBEUAHUwS0',
+ ytKey: 'AIzaSyB3IzJp56L5bzbuPJtmTmFv9R7U-eWlY2I',
+ prefix: '-',
+ statusBOT: 'Estoy listo!',
+ categories: [
+  {name: "general", priority: 5},
+  {name: "admin", priority: 8},
+  {name: "music", priority: 7}
+ ],
+ groups: [
+  {name: "User", permlvl: 0},
+  {name: "Member", permlvl: 1},
+  {name: "Mod", permlvl: 2},
+  {name: "Admin", permlvl: 3}
+ ]
+ 
+}
